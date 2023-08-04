4406a8e63034825e05e1a16f6fe1aaed3e345b7b
David Roussov <david.roussov@gmail.com>
2023-08-04 14:35:27 +1000

    sort repositories

--- app/Services/GithubService.php
+++ app/Services/GithubService.php
@@ -16,15 +16,8 @@ class GithubService {
     $filter = $search['filter'] ?? '';
     Log::debug("filter: $filter");
 
-    $order = ($search['sortOrder'] ?? '') === 'asc' ? 'asc' : 'desc';
-    Log::debug("order: $order");
-
-    $allowedSort = ['stars', 'forks', 'help-wanted-issues', 'updated'];
-    $sortField = $search['sortField'];
-    $sort = isset($allowedSort[$sortField]) ? $sortField : 'stars';
-
-    $pageSize = $search['pageSize'] ?? DEFAULT_PAGE_SIZE;
-    $perPage = $pageSize > 100 ? DEFAULT_PAGE_SIZE : $pageSize;
+    $pageSize = $search['pageSize'] ?? GithubService::DEFAULT_PAGE_SIZE;
+    $perPage = $pageSize > 100 ? GithubService::DEFAULT_PAGE_SIZE : $pageSize;
     Log::debug("perPage: $perPage");
 
     $page = $search['pageNumber'] ?? 1;
@@ -35,8 +28,6 @@ class GithubService {
 
     $queryString = http_build_query([
       'q' => $query,
-      'sort' => $sort,
-      'order' => $order,
       'per_page' => $perPage,
       'page' => $page,
     ]);
@@ -47,7 +38,6 @@ class GithubService {
       'base_uri' => GithubService::GITHUB_BASE_URL,
       'headers' => [
         'Accept' => 'application/json',
-        'Authorization' => 'Bearer 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',
         'X-GitHub-Api-Version' => '2022-11-28'
       ],
     ]);
@@ -55,29 +45,48 @@ class GithubService {
     $response = $client->get($path . '?' . $queryString);
     $data = json_decode($response->getBody(), true);
 
-    $repositories = collect($data['items'])->map(function ($item) {
-      return [
-        'id' => $item['id'],
-        'name' => $item['name'],
-        'full_name' => $item['full_name'],
-        'html_url' => $item['html_url'],
-        'language' => $item['language'],
-        'updated_at' => $item['updated_at'],
-        'pushed_at' => $item['pushed_at'],
-        'stargazers_count' => $item['stargazers_count'],
-      ];
-    });
+    $sortOrder = ($search['sortOrder'] ?? '') === 'asc' ? 'asc' : 'desc';
+    Log::debug("sortOrder: $sortOrder");
+
+    $allowedSort = ['name', 'stargazers_count', 'updated_at'];
+    $sortField = $search['sortField'];
+    $sort = isset($allowedSort[$sortField]) ? $sortField : 'name';
+    Log::debug("sort: $sort");
+
+    $repositories = collect($data['items'])
+      ->map(function ($item) {
+        return [
+          'id' => $item['id'],
+          'name' => $item['name'],
+          'full_name' => $item['full_name'],
+          'html_url' => $item['html_url'],
+          'language' => $item['language'],
+          'updated_at' => $item['updated_at'],
+          'pushed_at' => $item['pushed_at'],
+          'stargazers_count' => $item['stargazers_count'],
+        ];
+      });
+
+    $sortedRepositories = GithubService::sortRepositories($repositories, $sort, $sortOrder);
 
     return [
-      'repositories' => $repositories,
+      'repositories' => $sortedRepositories->values()->all(),
       'meta' => [
         'total' => $data['total_count'],
         'filter' => $filter,
-        'sortOrder' => $order,
+        'sortOrder' => $sortOrder,
         'sortField' => $sort,
         'pageSize' => $perPage,
         'pageNumber' => $page,
       ],
     ];
+  }
+
+  private static function sortRepositories($repositories, $sortField, $sortOrder) {
+    if ($sortOrder === 'asc') {
+      return $repositories->sortBy($sortField);
+    } else {
+      return $repositories->sortByDesc($sortField);
+    }
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'repositories'], function() {
+Route::group(['prefix' => 'repos'], function() {
   Route::get('', [RepositoryController::class, 'listRepositories']);
 });
